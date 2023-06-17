e05e86ced1088f3f10740d8f681a1999fd2a2158
Tom Pierce <tompierce4@gmail.com>
2023-06-17 21:29:34 +0100

    Player can enter rock paper or scissor and the game plays out
    Player is prompted to enter rock paper or scissors, their answer is evaluated through a switch statement to see if they lost or not against the computers random selection

--- javascript.js
+++ javascript.js
@@ -15,8 +15,57 @@ function getComputerChoice() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+
+    switch (computerSelection) {
+        case "rock":
+            if (playerSelection == "paper") {
+                return (playerWins(playerSelection, computerSelection));
+            }
+            else {
+                return (playerLoses(playerSelection, computerSelection));
+            }
+            break;
+
+        case "paper":
+            if (playerSelection == "scissors") {
+                return (playerWins(playerSelection, computerSelection));
+            }
+            else {
+                return (playerLoses(playerSelection, computerSelection));
+            }
+
+            break;
+
+        case "scissors":
+            if (playerSelection == "rock") {
+                return (playerWins(playerSelection, computerSelection));
+            }
+            else {
+                return (playerLoses(playerSelection, computerSelection));
+            }
+
+            break;
+    }
+
+}
+
+function playerWins(playerSelection, computerSelection) {
+    return ("You win! " + playerSelection + " beats " + computerSelection);
+}
+
+function playerLoses(playerSelection, computerSelection) {
+    return ("You lose! " + computerSelection + " beats " + playerSelection);
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Please enter your choice", "");
+playRound(playerSelection.toLowerCase(), computerSelection);
+
+
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
-console.log(getComputerChoice());
+console.log("Computer chose " + computerSelection);
+console.log("You chose " + playerSelection);
+console.log(playRound(playerSelection, computerSelection));
